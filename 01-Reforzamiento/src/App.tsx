bb9151b320bcc946e8c95c8848f409d635739345
andrescn2020 <andrescn_2010@hotmail.com>
2022-09-19 19:51:35 -0300

    Hook useState

--- 01-Reforzamiento/src/App.tsx
+++ 01-Reforzamiento/src/App.tsx
@@ -1,15 +1,17 @@
 // import { TiposBasicos } from "./typescript/TiposBasicos"
 // import { ObjetosLiterales } from "./typescript/ObjetosLiterales"
-import { Funciones } from './typescript/Funciones';
+// import { Funciones } from './typescript/Funciones';
+import { Contador } from "./components/Contador"
 
 const App = () => {
   return (
     <div className='mt-2'>
       <h1>Introducción a TS React</h1>
       <hr />
-      {/* <TiposBasicos/> */}
-      {/* <ObjetosLiterales/> */}
-      <Funciones />
+      {/* <TiposBasicos /> */}
+      {/* <ObjetosLiterales /> */}
+      {/* <Funciones /> */}
+      <Contador />
     </div>
   )
 }
