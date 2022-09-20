2a8c87641b11698e796ea2db1de83b6a154bf312
andrescn2020 <andrescn_2010@hotmail.com>
2022-09-20 18:28:18 -0300

    Custom hook useForm

--- 01-Reforzamiento/src/App.tsx
+++ 01-Reforzamiento/src/App.tsx
@@ -4,7 +4,8 @@
 // import { Contador } from "./components/Contador"
 // import { ContadorConHook } from "./components/ContadorConHook"
 // import { Login } from "./components/Login"
-import { Usuarios } from "./components/Usuarios"
+// import { Usuarios } from "./components/Usuarios"
+import { Formulario } from "./components/Formulario"
 
 const App = () => {
   return (
@@ -17,7 +18,8 @@ const App = () => {
       {/* <Contador /> */}
       {/* <ContadorConHook /> */}
       {/* <Login /> */}
-      <Usuarios />
+      {/* <Usuarios /> */}
+      <Formulario />
     </div>
   )
 }
