11c0fb30b7447e5752282ea9a8f1242405744d01
andrescn2020 <andrescn_2010@hotmail.com>
2022-09-26 19:31:23 -0300

    Primera configuracion y estilos

---
+++ CalculadoraIOS/src/screens/CalculadoraScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+import { styles } from '../theme/appTheme';
+
+const CalculadoraScreen = () => {
+  return (
+    <View>
+        <Text style={styles.texto}>Calculadora Screen</Text>
+    </View>
+  )
+}
+
+export default CalculadoraScreen
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # react-native-journey
+# react-native-journey
