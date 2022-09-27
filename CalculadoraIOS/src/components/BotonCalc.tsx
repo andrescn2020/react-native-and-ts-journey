588c597abaa172f44ba982270c44e7143986f8df
andrescn2020 <andrescn_2010@hotmail.com>
2022-09-26 21:45:44 -0300

    Logica en el ingreso de datos

--- CalculadoraIOS/src/components/BotonCalc.tsx
+++ CalculadoraIOS/src/components/BotonCalc.tsx
@@ -5,12 +5,13 @@ import { styles } from '../theme/AppTheme';
 interface Props {
     texto: string,
     color?: string,
-    ancho?: boolean
+    ancho?: boolean,
+    accion: (numeroTexto:string) => void
 }
 
-const BotonCalc = ({ texto, color = "#2D2D2D", ancho = false }: Props) => {
+const BotonCalc = ({ texto, color = "#2D2D2D", ancho = false, accion }: Props) => {
     return (
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => accion(texto)}>
             <View style={{
                 ...styles.boton,
                 backgroundColor: color,
--- CalculadoraIOS/src/screens/CalculadoraScreen.tsx
+++ CalculadoraIOS/src/screens/CalculadoraScreen.tsx
@@ -2,46 +2,81 @@ import React from 'react'
 import { View, Text } from 'react-native';
 import { styles } from '../theme/AppTheme';
 import BotonCalc from '../components/BotonCalc';
-
+import { useState } from 'react';
 
 const CalculadoraScreen = () => {
+
+    const [numeroAnterior, setNumeroAnterior] = useState("0");
+    const [numero, setNumero] = useState("0");
+
+    const limpiar = () => {
+        setNumero("0")
+    }
+
+    const armarNumero = (numeroTexto: string) => {
+        if(numero.includes(".") && numeroTexto === ".") return;
+        if(numero.startsWith("0") || numero.startsWith("-0")) {
+            if(numeroTexto === "."){
+                setNumero(numero + numeroTexto);
+            } else if(numeroTexto === "0" && numero.includes(".")){
+                setNumero(numero + numeroTexto);
+            } else if(numeroTexto !== "0" && !numero.includes(".")){
+                setNumero(numeroTexto);
+            } else if(numeroTexto === "0" && !numero.includes(".")){
+                setNumero(numero)
+            } else {
+                setNumero(numero + numeroTexto);
+            }
+        } else {
+            setNumero(numero + numeroTexto);
+        }
+    }
+
+    const positivoNegativo = () => {
+        if (numero.includes("-")){
+            setNumero(numero.replace("-", ""))
+        } else {
+            setNumero( "-" + numero)
+        }
+    }
+
     return (
         <View style={styles.calculadoraContainer}>
-            <Text style={styles.resultadoPequenio}>1,500.00</Text>
-            <Text style={styles.resultado}>1,500.00</Text>
+            <Text style={styles.resultadoPequenio}>{numeroAnterior}</Text>
+            <Text style={styles.resultado} numberOfLines={1} adjustsFontSizeToFit>{numero}</Text>
             {/* Fila de botones */}
             <View style={styles.fila}>
-                <BotonCalc texto="C" color="#9B9B9B" />
-                <BotonCalc texto="+/-" color="#9B9B9B" />
-                <BotonCalc texto="del" color="#9B9B9B" />
-                <BotonCalc texto="/" color="#FF9427" />
+                <BotonCalc texto="C" color="#9B9B9B" accion={limpiar} />
+                <BotonCalc texto="+/-" color="#9B9B9B" accion={positivoNegativo}/>
+                <BotonCalc texto="del" color="#9B9B9B" accion={limpiar}/>
+                <BotonCalc texto="/" color="#FF9427" accion={limpiar}/>
             </View>
             {/* Fila de botones */}
             <View style={styles.fila}>
-                <BotonCalc texto="7" />
-                <BotonCalc texto="8" />
-                <BotonCalc texto="9"/>
-                <BotonCalc texto="X" color="#FF9427" />
+                <BotonCalc texto="7" accion={armarNumero}/>
+                <BotonCalc texto="8" accion={armarNumero}/>
+                <BotonCalc texto="9" accion={armarNumero}/>
+                <BotonCalc texto="X" color="#FF9427" accion={limpiar}/>
             </View>
             {/* Fila de botones */}
             <View style={styles.fila}>
-                <BotonCalc texto="4" />
-                <BotonCalc texto="5" />
-                <BotonCalc texto="6"/>
-                <BotonCalc texto="-" color="#FF9427" />
+                <BotonCalc texto="4" accion={armarNumero}/>
+                <BotonCalc texto="5" accion={armarNumero}/>
+                <BotonCalc texto="6" accion={armarNumero}/>
+                <BotonCalc texto="-" color="#FF9427" accion={limpiar}/>
             </View>
             {/* Fila de botones */}
             <View style={styles.fila}>
-                <BotonCalc texto="1" />
-                <BotonCalc texto="2" />
-                <BotonCalc texto="3"/>
-                <BotonCalc texto="+" color="#FF9427" />
+                <BotonCalc texto="1" accion={armarNumero}/>
+                <BotonCalc texto="2" accion={armarNumero}/>
+                <BotonCalc texto="3" accion={armarNumero}/>
+                <BotonCalc texto="+" color="#FF9427" accion={limpiar}/>
             </View>
             {/* Fila de botones */}
             <View style={styles.fila}>
-                <BotonCalc texto="0" ancho />
-                <BotonCalc texto="."/>
-                <BotonCalc texto="=" color="#FF9427" />
+                <BotonCalc texto="0" ancho accion={armarNumero}/>
+                <BotonCalc texto="." accion={armarNumero}/>
+                <BotonCalc texto="=" color="#FF9427" accion={limpiar}/>
             </View>
         </View>
     )
