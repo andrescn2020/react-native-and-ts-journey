e6256e149f602ba1b090e48863e3e504cc675bea
andrescn2020 <andrescn_2010@hotmail.com>
2022-09-28 14:46:10 -0300

    Configuracion inicial del proyecto y ruteo

---
+++ NavegacionApp/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Text>Navegacion Apps</Text>
+    </NavigationContainer>
+  )
+}
+
+export default App;
