cd771f522ab70b6820f7eee4a858686efb8f73dc
andrescn2020 <andrescn_2010@hotmail.com>
2022-10-03 11:23:06 -0300

    Sign In

--- NavegacionApp/src/screens/ContacsScreens.tsx
+++ NavegacionApp/src/screens/ContacsScreens.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import { View, Text } from 'react-native';
+import React, { useContext } from 'react'
+import { View, Text, Button } from 'react-native';
+import { styles } from '../theme/appTheme';
+import { AuthContext } from '../context/AuthContext';
 
 const ContacsScreens = () => {
+
+  const { signIn, authState: {isLoggedIn} } = useContext(AuthContext)
+
+
   return (
-    <View>
-        <Text>Contacts Screens</Text>
+    <View style={styles.globalMargin}>
+        <Text style={styles.title}>Contacts Screens</Text>
+        {isLoggedIn ? <></> :<Button title='SignIn' onPress={signIn}/>}
     </View>
   )
 }
