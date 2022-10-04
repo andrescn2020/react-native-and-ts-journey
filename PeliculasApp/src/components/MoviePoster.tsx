8f4c854a35b813fb2ba4c6eae533ddd92f93a770
andrescn2020 <andrescn_2010@hotmail.com>
2022-10-04 19:04:19 -0300

    Navegar a detalles

--- PeliculasApp/src/components/MoviePoster.tsx
+++ PeliculasApp/src/components/MoviePoster.tsx
@@ -1,6 +1,8 @@
+import { useNavigation } from '@react-navigation/core';
 import React from 'react'
 import { View, Image, StyleSheet } from 'react-native';
 import { Movie } from '../interfaces/movieInterface';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 interface Props {
     movie: Movie;
@@ -10,10 +12,15 @@ interface Props {
 
 const MoviePoster = ({ movie, height = 420, width = 300 }: Props) => {
 
-    const uri = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+    const uri = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+
+    const navigation = useNavigation();
 
     return (
-        <View style={{
+        <TouchableOpacity 
+        onPress={() => navigation.navigate('DetailScreen' as never, movie as never)}
+        activeOpacity={0.8}
+        style={{
             width,
             height,
             marginHorizontal: 8
@@ -23,7 +30,7 @@ const MoviePoster = ({ movie, height = 420, width = 300 }: Props) => {
                     source={{ uri }}
                     style={styles.image} />
             </View>
-        </View>
+        </TouchableOpacity>
     )
 }
 
--- PeliculasApp/src/screens/DetailScreen.tsx
+++ PeliculasApp/src/screens/DetailScreen.tsx
@@ -1,7 +1,17 @@
+import { StackScreenProps } from '@react-navigation/stack';
 import React from 'react'
 import { View, Text } from 'react-native';
+// import { Movie } from '../interfaces/movieInterface';
+import { RootStackParams } from '../navigation/Navigation';
 
-const DetailScreen = () => {
+interface Props extends StackScreenProps<RootStackParams, 'DetailScreen'> {};
+
+const DetailScreen = ({route}: Props) => {
+
+  const movie = route.params;
+
+  console.log(movie.original_title);
+  
   return (
     <View>
         <Text>Detail</Text>
