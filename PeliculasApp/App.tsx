93ad791a14866957c3f93c91313b9b9f5fa600da
andrescn2020 <andrescn_2010@hotmail.com>
2022-10-10 18:48:33 -0300

    Creación del Gradiant Context

--- PeliculasApp/App.tsx
+++ PeliculasApp/App.tsx
@@ -2,11 +2,22 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 //import FadeInScreen from './src/screens/FadeInScreen';
 import { Navigation } from './src/navigation/Navigation';
+import { GradientProvider } from './src/context/gradientContext';
+
+const AppState = ({ children }: any) => {
+  return (
+    <GradientProvider>
+      {children}
+    </GradientProvider>
+  )
+}
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Navigation />
+      <AppState>
+        <Navigation />
+      </AppState>
       {/* <FadeInScreen/> */}
     </NavigationContainer>
   )
